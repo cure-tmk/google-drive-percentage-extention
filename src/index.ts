c7ab47366b600ed36e7aa66c0e99f5f0b83f0d05
cure-tomoki <cure.tomoki@gmail.com>
2020-09-19 21:31:04 +0900

    render circle

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,62 @@ const PROGRESS_INDICATOR_SELECTOR =
 let progressIndicators: Element[] = [];
 let progressObservers: MutationObserver[] = [];
 
+const renderProgress = (percentage: number): string => {
+  const size = 40;
+  const strokeWidth = 2;
+  const radius = size / 2 - strokeWidth * 2;
+  const circum = radius * 2 * Math.PI;
+  const offset = circum - (percentage / 100) * circum;
+  return `
+  <svg
+    style="position:absolute; top:50%; left:50%; transform:translate(-50%,-50%); width:${size}; height: ${size};"
+    height="${size}"
+    width="${size}"
+  >
+    <text
+      x="50%"
+      y="50%"
+      text-anchor="middle"
+      dy=".35em"
+      style="font-size: 12px; color:#757575; opacity: 0.5;"
+    >${percentage}%</text>
+    <circle
+      stroke="#757575"
+      stroke-width="${strokeWidth}"
+      fill="transparent"
+      r="${radius}"
+      cx="${size / 2}"
+      cy="${size / 2}"
+      style="opacity: 0.5"
+    />
+    <circle
+      stroke="#4285f4"
+      stroke-width="${strokeWidth}"
+      stroke-dasharray="${circum} ${circum}"
+      stroke-dashoffset="${offset}"
+      fill="transparent"
+      r="${radius}"
+      cx="${size / 2}"
+      cy="${size / 2}"
+      transform="rotate(-90,${size / 2},${size / 2})"
+    />
+  </svg>
+  `;
+};
+
 const handleMutation: MutationCallback = (mutations) => {
-  const current = mutations[mutations.length - 1];
+  const current = mutations[mutations.length - 1]?.target as
+    | HTMLElement
+    | undefined;
+  if (current === undefined) {
+    return;
+  }
   // @ts-ignore aria-valuenow is not on type Node
-  const ariaValueNow = current.target.ariaValueNow as number | undefined;
-  const value = ariaValueNow !== undefined ? ariaValueNow : 0;
-  console.log(value);
+  const ariaValueNow = current.ariaValueNow as number | undefined;
+  const percentage = ariaValueNow !== undefined ? ariaValueNow : 0;
+  current.innerHTML = renderProgress(percentage);
+  current.style.animation = 'none'; // prevent spinning
+  current.style.position = 'relative';
 };
 
 const attachEventHandlers = (elements: Element[]) => {
