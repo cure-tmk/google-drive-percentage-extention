068bd1b319cdfd47afb30364db1ab79afb99577c
cure-tomoki <cure.tomoki@gmail.com>
2020-09-19 17:27:18 +0900

    gets ariaValueNow

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,48 @@
 import isEqual from 'lodash/isEqual';
 
 const PROGRESS_INDICATOR_SELECTOR =
-  '[data-progress][aria-valuemin="0"][aria-valuemax="100"]';
+  'div[data-progress][aria-valuemin="0"][aria-valuemax="100"]';
 
 // global states
-let progressIndicators: Element[];
+let progressIndicators: Element[] = [];
+let progressObservers: MutationObserver[] = [];
 
-const bodyObserver = new MutationObserver((_mutations) => {
-  const progressIndicatorElements = document.querySelectorAll(
-    PROGRESS_INDICATOR_SELECTOR
-  );
-  const elements = Array.from(progressIndicatorElements);
-  if (isEqual(progressIndicators, elements)) {
+const handleMutation: MutationCallback = (mutations) => {
+  const current = mutations[mutations.length - 1];
+  // @ts-ignore aria-valuenow is not on type Node
+  const ariaValueNow = current.target.ariaValueNow as number | undefined;
+  const value = ariaValueNow !== undefined ? ariaValueNow : 0;
+  console.log(value);
+};
+
+const attachEventHandlers = (elements: Element[]) => {
+  if (elements.length === 0) {
     return;
   }
-  console.log(elements);
-  progressIndicators = elements;
-});
+  progressObservers = elements.map((e) => {
+    const Observer = new MutationObserver(handleMutation);
+    Observer.observe(e, {
+      childList: false,
+      subtree: false,
+      attributes: true,
+      characterData: false,
+    });
+    return Observer;
+  });
+};
 
 const run = () => {
+  const bodyObserver = new MutationObserver(() => {
+    const progressIndicatorElements = document.querySelectorAll(
+      PROGRESS_INDICATOR_SELECTOR
+    );
+    const elements = Array.from(progressIndicatorElements);
+    if (isEqual(progressIndicators, elements)) {
+      return;
+    }
+    progressIndicators = elements;
+    attachEventHandlers(elements);
+  });
   bodyObserver.observe(document.body, {
     childList: true,
     subtree: true,
@@ -29,6 +53,9 @@ const run = () => {
   // cleanup
   window.onbeforeunload = () => {
     bodyObserver.disconnect();
+    progressObservers.forEach((mo) => {
+      mo.disconnect();
+    });
   };
 };
 
