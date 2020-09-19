9343e76e0f28ed7edfba1546c7fa6260c618446f
cure-tomoki <cure.tomoki@gmail.com>
2020-09-19 16:43:21 +0900

    listens for progressIndicators

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,41 @@
+import isEqual from 'lodash/isEqual';
+
+const PROGRESS_INDICATOR_SELECTOR =
+  '[data-progress][aria-valuemin="0"][aria-valuemax="100"]';
+
+// global states
+let progressIndicators: Element[];
+
+const bodyObserver = new MutationObserver((_mutations) => {
+  const progressIndicatorElements = document.querySelectorAll(
+    PROGRESS_INDICATOR_SELECTOR
+  );
+  const elements = Array.from(progressIndicatorElements);
+  if (isEqual(progressIndicators, elements)) {
+    return;
+  }
+  console.log(elements);
+  progressIndicators = elements;
+});
+
+const run = () => {
+  bodyObserver.observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: false,
+    characterData: false,
+  });
+
+  // cleanup
+  window.onbeforeunload = () => {
+    bodyObserver.disconnect();
+  };
+};
+
 (() => {
-  console.log("foo");
+  if (document.readyState === 'complete') {
+    run();
+  } else {
+    document.addEventListener('DOMContentLoaded', run);
+  }
 })();
